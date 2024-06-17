8edb6d2022a20d4845d1b67f5accf969784fe45a
aleena02122002 <khananna919@gmail.com>
2024-06-17 18:42:35 +0500

    Changes in card scaffold

--- lib/utils/Container.dart
+++ lib/utils/Container.dart
@@ -49,55 +49,99 @@ class HomeContainer extends StatelessWidget {
     );
   }
 }
-
 class CardsContainer extends StatelessWidget {
   final String amount;
   final String percentage;
   final String label;
-  final percentageColor;
+  final String expDate;
+  final Color percentageColor;
 
-  const CardsContainer({super.key, required this.amount,required this.percentage,required this.label,required this.percentageColor});
-
+  const CardsContainer({
+    Key? key,
+    required this.amount,
+    required this.percentage,
+    required this.label,
+    required this.percentageColor,
+    required this.expDate,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 8.0,vertical: 8.0),
+      padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
       child: Container(
-        height: 250,
+        height: 260,
         width: 200,
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(30),
-          color: Colors.white.withOpacity(0.1),
+          color: percentageColor,
         ),
-        child: Row(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 7.0, horizontal: 10.0),
-                  child: Column(
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 7.0, horizontal: 10.0),
+              child: Column(
+                children: [
+                  Row(
                     children: [
-                      Text(amount, style: TextStyle(color: Colors.white)),
-                      Text(percentage, style: TextStyle(color: percentageColor)),
+                      Image(image: AssetImage("assets/images/debitlogo.png"),height: 40,width: 40,),
+                      SizedBox(width: 75,),
+                      Text(expDate, style: TextStyle(color: Colors.white)),
                     ],
                   ),
-                ),
-                SizedBox(height: 70),
-                Text(label, style: TextStyle(color: Colors.white)),
-              ],
-            ),
-            SizedBox(width: 35, height: 10),
-            Align(
-              alignment: Alignment.topRight,
-              child: ClipOval(
-                child: Image.asset("assets/images/arrow.png", width: 40, height: 40),
+                  SizedBox(height: 10),
+                  Text(percentage, style: TextStyle(color: Colors.white)),
+                ],
               ),
             ),
+
+               Align(
+                alignment: Alignment.bottomLeft,
+                child: Column(
+                  children: [
+
+                    Text(label, style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold)),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 15.0,vertical: 5.0),
+                      child: Text(amount, style: TextStyle(color: Colors.white, fontSize: 20)),
+                    ),
+                  ],
+                ),
+              ),
+
           ],
         ),
       ),
     );
   }
 }
+class CardListContainer extends StatelessWidget {
+  const CardListContainer({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 90,
+      width: 90,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(30),
+        color: Colors.grey.withOpacity(0.2),
+      ),
+      child: Center(
+        child: Container(
+          height: 60, // Adjust height as needed
+          width: 60,  // Adjust width as needed
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30),
+            color: Colors.grey,
+          ),
+          child: Icon(
+            Icons.shopping_cart_outlined,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/views/Cards.dart
+++ lib/views/Cards.dart
@@ -1,3 +1,4 @@
+import 'package:application/utils/Container.dart';
 import 'package:flutter/material.dart';
 
 class Cards extends StatelessWidget {
@@ -5,22 +6,42 @@ class Cards extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      extendBodyBehindAppBar: true,
-      backgroundColor: Colors.white38,
-      appBar: AppBar(
-        leading: Text("My Cards"),
-        actions: [
-          Container(
-            height: 60,
-            width: 60,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(30),
-              color: Colors.white.withOpacity(0.2),
+    return SafeArea(
+      child: Scaffold(
+        extendBodyBehindAppBar: true,
+        backgroundColor: Color(0xFFe0e9f1),
+        appBar: AppBar(
+          forceMaterialTransparency: true,
+          leading: Text("Cards",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18,color: Colors.black38),),
+          actions: [
+            Container(
+              height: 60,
+              width: 60,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                color: Colors.white.withOpacity(0.2),
+              ),
+              child: Icon(Icons.arrow_back, color: Colors.black),
             ),
-            child: Icon(Icons.arrow_back, color: Colors.black),
+          ],
+        ),
+        body: SafeArea(
+          child: Column(
+            children: [
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    CardsContainer(amount: "Rs 15,000.00", percentage: "8426 9823 0957 3674", label: "Balance", percentageColor: Color(0xFF1F2F3C),expDate: "12/24",),
+                    CardsContainer(amount: "Rs 15,000.00", percentage: "8426 9823 0957 3674", label: "Balance", percentageColor: Colors.grey.shade400,expDate: "09/25",),
+                  ],
+                ),
+              ),
+              Text("Transactions"),
+              CardListContainer(),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
